8e4ae5d20ece62802bbd43868c0b49fbb6e4f753
Hanna Söderström <gothbarbie84@gmail.com>
2018-11-17 15:44:32 +0100

    refactor: extract backdrop and wrapper to Modal component

--- client/src/components/DeleteModal/DeleteModal.js
+++ client/src/components/DeleteModal/DeleteModal.js
@@ -1,25 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+import { deleteNoteAndHideModalThunk } from '../../actions/notesActions'
+import { hideModal } from '../../actions/modalActions'
 
 import Button from '../Button'
-
-const DeleteModalBackdrop = styled.div`
-  position: fixed;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.7);
-`
-
-const ModalWrapper = styled.section`
-  background-color: #fff;
-  border-radius: 4px;
-  padding: 3rem 2rem;
-`
 
 const Title = styled.h2`
   font-size: 1.6rem;
@@ -47,21 +34,54 @@ const ButtonsWrapper = styled.div`
   grid-gap: 3rem;
 `
 
-const DeleteModal = ({ title, onDelete }) => (
-  <DeleteModalBackdrop>
-    <ModalWrapper>
-      <Title>Title</Title>
-      <Text>
-        Are you sure you want to <span>delete</span> this note?
-      </Text>
-      <ButtonsWrapper>
-        <Button>Cancel</Button>
-        <Button onClick={onDelete} type="red">
-          Delete
-        </Button>
-      </ButtonsWrapper>
-    </ModalWrapper>
-  </DeleteModalBackdrop>
+const DeleteModal = ({
+  deleteNoteAndHideModalThunk,
+  hideModal,
+  note,
+  deleteNoteId,
+}) => (
+  <div>
+    <Title>{note.title}</Title>
+    <Text>
+      Are you sure you want to <span>delete</span> this note?
+    </Text>
+    <ButtonsWrapper>
+      <Button onClick={hideModal}>Cancel</Button>
+      <Button
+        onClick={() => deleteNoteAndHideModalThunk({ id: deleteNoteId })}
+        type="red"
+      >
+        Delete
+      </Button>
+    </ButtonsWrapper>
+  </div>
 )
 
-export default DeleteModal
+DeleteModal.propTypes = {
+  deleteNoteAndHideModalThunk: PropTypes.func.isRequired,
+  deleteNoteId: PropTypes.string.isRequired,
+  hideModal: PropTypes.func.isRequired,
+  note: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    timestamp: PropTypes.instanceOf(Date).isRequired,
+    description: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+  }).isRequired,
+}
+
+const mapStateToProps = state => {
+  const deleteNoteId = state.modal.deleteNoteId
+  const note = state.notes[deleteNoteId]
+  return { note, deleteNoteId }
+}
+
+const mapDispatchToProps = {
+  hideModal,
+  deleteNoteAndHideModalThunk,
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DeleteModal)
