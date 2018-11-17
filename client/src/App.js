32098e3067833baa15fdba4b898702befe6ee7d3
Hanna Söderström <gothbarbie84@gmail.com>
2018-11-17 13:46:16 +0100

    feat: create and render notes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+
+import { addNoteThunk } from './actions/notesActions'
 
 import NewNoteButton from './components/NewNoteButton'
 import SearchField from './components/SearchField'
@@ -6,55 +10,59 @@ import NoteItem from './components/NoteItem'
 import Normalizer from './components/Normalizer'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    props.addNoteThunk()
+    props.addNoteThunk()
+    props.addNoteThunk()
+    props.addNoteThunk()
+  }
+
+  renderNotes() {
+    const { notes } = this.props
+
+    return Object.entries(notes)
+      .reverse()
+      .map(([id, note]) => {
+        return <NoteItem key={id} note={{ id, ...note }} />
+      })
+  }
+
   render() {
     return (
       <div>
         <Normalizer />
-        <NewNoteButton />
+        <NewNoteButton onClick={this.props.addNoteThunk} />
         <SearchField />
-        <NoteItem
-          note={{
-            id: 'n01',
-            title: 'Title',
-            description: 'Description',
-            category: 'News',
-            content: 'Content',
-            datestamp: '2018-01-01',
-          }}
-        />
-        <NoteItem
-          note={{
-            id: 'n01',
-            title: 'Title',
-            description: 'Description',
-            category: 'News',
-            content: 'Content',
-            datestamp: '2018-01-01',
-          }}
-        />
-        <NoteItem
-          note={{
-            id: 'n01',
-            title: 'Title',
-            description: 'Description',
-            category: 'News',
-            content: 'Content',
-            datestamp: '2018-01-01',
-          }}
-        />
-        <NoteItem
-          note={{
-            id: 'n01',
-            title: 'Title',
-            description: 'Description',
-            category: 'News',
-            content: 'Content',
-            datestamp: '2018-01-01',
-          }}
-        />
+        {this.renderNotes()}
       </div>
     )
   }
 }
 
-export default App
+App.propTypes = {
+  addNoteThunk: PropTypes.func.isRequired,
+  notes: PropTypes.shape({
+    [PropTypes.string.isRequired]: {
+      title: PropTypes.string.isRequired,
+      timestamp: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+    },
+  }).isRequired,
+}
+
+const mapStateToProps = ({ notes }) => ({
+  notes,
+})
+
+const mapDispatchToProps = {
+  addNoteThunk,
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
