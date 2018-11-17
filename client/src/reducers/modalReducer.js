9ac38f20232865c1cfd3657a652148a75f035965
Hanna Söderström <gothbarbie84@gmail.com>
2018-11-17 15:46:01 +0100

    feat: handle SET_DELETE_NOTE_ID

--- client/src/reducers/modalReducer.js
+++ client/src/reducers/modalReducer.js
@@ -1,19 +1,32 @@
-import { SHOW_MODAL, HIDE_MODAL } from '../actions/modalActions'
+import {
+  SHOW_MODAL,
+  HIDE_MODAL,
+  SET_DELETE_NOTE_ID,
+} from '../actions/modalActions'
 
 const initialState = {
   show: false,
+  deleteNoteId: '',
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case SHOW_MODAL:
       return {
+        ...state,
         show: true,
       }
 
     case HIDE_MODAL:
       return {
+        ...state,
         show: false,
+      }
+
+    case SET_DELETE_NOTE_ID:
+      return {
+        ...state,
+        deleteNoteId: action.payload.deleteNoteId,
       }
 
     default:
