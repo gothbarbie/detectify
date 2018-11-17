f6c003bd181af89a423284288489f3142ae6f6f3
Hanna Söderström <gothbarbie84@gmail.com>
2018-11-17 15:44:59 +0100

    refactor: add backdrop and wrapper

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,8 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const ModalBackdrop = styled.div`
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+`
+
+const ModalWrapper = styled.section`
+  background-color: #fff;
+  border-radius: 4px;
+  padding: 3rem 2rem;
+`
 
 const Modal = ({ show, children }) => {
-  return show ? <div>{children}</div> : null
+  return show ? (
+    <ModalBackdrop>
+      <ModalWrapper>{children}</ModalWrapper>
+    </ModalBackdrop>
+  ) : null
 }
 
 Modal.propTypes = {
