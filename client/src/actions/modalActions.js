ac70e9f3f677b79a1dfa8c320d9af8218fb82f19
Hanna Söderström <gothbarbie84@gmail.com>
2018-11-17 15:43:56 +0100

    feat: handle setDeleteNoteId and showModalAndSetDeleteNoteIdThunk

--- client/src/actions/modalActions.js
+++ client/src/actions/modalActions.js
@@ -1,10 +1,23 @@
 export const SHOW_MODAL = 'SHOW_MODAL'
 export const HIDE_MODAL = 'HIDE_MODAL'
+export const SET_DELETE_NOTE_ID = 'SET_DELETE_NOTE_ID'
 
-export const showModal = () => ({
+const showModal = () => ({
   type: SHOW_MODAL,
 })
 
 export const hideModal = () => ({
   type: HIDE_MODAL,
 })
+
+const setDeleteNoteId = ({ deleteNoteId }) => ({
+  type: SET_DELETE_NOTE_ID,
+  payload: { deleteNoteId },
+})
+
+export const showModalAndSetDeleteNoteIdThunk = ({
+  deleteNoteId,
+}) => dispatch => {
+  dispatch(showModal())
+  dispatch(setDeleteNoteId({ deleteNoteId }))
+}
