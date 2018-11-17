fc3ee47737dd545b10e154b0fbcbb561364c8e76
Hanna Söderström <gothbarbie84@gmail.com>
2018-11-17 15:44:48 +0100

    test: update test, add icon

--- client/src/components/Icon/__tests__/Icon.spec.js
+++ client/src/components/Icon/__tests__/Icon.spec.js
@@ -5,7 +5,10 @@ import Icon from '../Icon'
 
 describe('Components/Icon', () => {
   it('renders', () => {
-    const wrapper = shallow(<Icon />)
+    const props = {
+      icon: ['fas', 'times'],
+    }
+    const wrapper = shallow(<Icon {...props} />)
 
     expect(wrapper).toMatchSnapshot()
   })
