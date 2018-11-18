11f6cb150f65c7122c5f9cfeb6bb997014b68be0
Hanna Söderström <gothbarbie84@gmail.com>
2018-11-18 12:32:44 +0100

    test: mock Date and handle note/new

--- client/src/Utils/__tests__/PretendAPI.spec.js
+++ client/src/Utils/__tests__/PretendAPI.spec.js
@@ -4,26 +4,66 @@ let API
 let id
 
 describe('Utils/PretendAPI', () => {
+  const RealDate = Date
+
+  function mockDate(isoDate) {
+    global.Date = class extends RealDate {
+      constructor() {
+        return new RealDate(isoDate)
+      }
+    }
+  }
+
+  afterEach(() => {
+    global.Date = RealDate
+  })
+
   beforeEach(() => {
     API = new PretendAPI()
     id = '4653eade-15ed-419a-b57f-6f169f97a687'
   })
 
-  it('stores data', () => {
+  it('/note - stores data', () => {
     const data = {
-      timestamp:
-        'Sun Nov 18 2018 11:43:07 GMT+0100 (centraleuropeisk normaltid)',
+      timestamp: mockDate('2017-11-25T12:34:56z'),
       title: 'New Note',
       category: 'Category',
       description: '',
       content: '',
     }
-    const result = API.post({ id, data })
+    const result = API.post('/note', { id, data })
     const expectedResult = {
-      body: id,
+      body: {
+        data: {
+          category: 'Category',
+          content: '',
+          description: '',
+          timestamp: mockDate('2017-11-25T12:34:56z'),
+          title: 'New Note',
+        },
+        id: '4653eade-15ed-419a-b57f-6f169f97a687',
+      },
       status: 200,
     }
     expect(result).toEqual(expectedResult)
+  })
+
+  it('/note/new - returns new note', () => {
+    const result = API.post('/note/new')
+
+    expect(result).toEqual({
+      body: {
+        data: {
+          category: 'Category',
+          content: '',
+          description: '',
+          timestamp: expect.anything(),
+          title: 'New Note',
+        },
+        id: expect.anything(),
+      },
+      status: 200,
+    })
   })
 
   it('retrieves data', () => {
@@ -33,8 +73,7 @@ describe('Utils/PretendAPI', () => {
         category: 'Category',
         content: '',
         description: '',
-        timestamp:
-          'Sun Nov 18 2018 11:43:07 GMT+0100 (centraleuropeisk normaltid)',
+        timestamp: '2017-11-25T12:34:56.000Z',
         title: 'New Note',
       },
       status: 200,
