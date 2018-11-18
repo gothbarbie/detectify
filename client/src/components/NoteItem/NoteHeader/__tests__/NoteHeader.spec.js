b06f4693c02b5a76bfd4ae96f7513d694ba5b640
Hanna Söderström <gothbarbie84@gmail.com>
2018-11-18 16:41:09 +0100

    test: update props and snapshots

--- client/src/components/NoteItem/NoteHeader/__tests__/NoteHeader.spec.js
+++ client/src/components/NoteItem/NoteHeader/__tests__/NoteHeader.spec.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 import NoteHeader from '../NoteHeader'
-describe('', () => {
-  it('renders', () => {
+
+describe('Components/NoteItem/NoteHeader', () => {
+  it('renders closed', () => {
     const props = {
       open: false,
       toggle: jest.fn(),
@@ -15,6 +16,38 @@ describe('', () => {
         category: 'News',
         datestamp: '2018-01-01',
       },
+      setEditTitle: jest.fn(),
+      title: 'Note Title',
+      timestamp: '',
+      description: '',
+      category: 'Note Category',
+      editTitle: false,
+      onChange: jest.fn(),
+      onBlur: jest.fn(),
+    }
+    const wrapper = shallow(<NoteHeader {...props} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+  it('renders open', () => {
+    const props = {
+      open: true,
+      toggle: jest.fn(),
+      note: {
+        id: 'n01',
+        title: 'Title',
+        description: 'Description',
+        content: 'Content',
+        category: 'News',
+        datestamp: '2018-01-01',
+      },
+      setEditTitle: jest.fn(),
+      title: 'Note Title',
+      timestamp: '',
+      description: '',
+      category: 'Note Category',
+      editTitle: false,
+      onChange: jest.fn(),
+      onBlur: jest.fn(),
     }
     const wrapper = shallow(<NoteHeader {...props} />)
     expect(wrapper).toMatchSnapshot()
