82456b56fd44ad50b0ac509f0cfdbae6a022b239
Hanna Söderström <gothbarbie84@gmail.com>
2018-11-18 13:13:22 +0100

    feat: implement PretendAPI

--- client/src/actions/notesActions.js
+++ client/src/actions/notesActions.js
@@ -1,10 +1,11 @@
-import uuid from 'uuid/v4'
-
 import { hideModal } from './modalActions'
+import PretendAPI from '../Utils/PretendAPI'
 
 export const ADD_NOTE = 'ADD_NOTE'
 export const UPDATE_NOTE = 'UPDATE_NOTE'
 export const DELETE_NOTE = 'DELETE_NOTE'
+
+const API = new PretendAPI()
 
 const addNote = ({ id, note }) => ({
   type: ADD_NOTE,
@@ -22,24 +23,33 @@ const deleteNote = ({ id }) => ({
 })
 
 export const addNoteThunk = () => dispatch => {
-  // 1. Request new Note from API
+  // Request new Note from API
+  const response = API.post('/note/new')
 
-  // 2. Add new Note to Store
-  const id = uuid()
-
-  dispatch(addNote({ id }))
+  dispatch(addNote({ id: response.body.id, note: response.body.note }))
 }
 
 export const updateNoteThunk = ({ id, note }) => dispatch => {
   // 1. Update API
-
+  const response = API.post('/note', { id, note })
+  console.log(response)
   // 2. Update note
   dispatch(updateNote({ id, note }))
 }
 
 export const deleteNoteAndHideModalThunk = ({ id }) => dispatch => {
   // 1. Delete in API
+  const response = API.delete(id)
+  console.log('delete', response)
   // 2. Delete from store
   dispatch(deleteNote({ id }))
   dispatch(hideModal())
+}
+
+export const loadSavedNotesThunk = () => dispatch => {
+  const response = API.getAll()
+
+  for (const id in response.body) {
+    dispatch(addNote({ id, note: response.body[id] }))
+  }
 }
