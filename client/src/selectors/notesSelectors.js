4f6886f93039b04e7eae6dc857813792b7ed3086
Hanna Söderström <gothbarbie84@gmail.com>
2018-11-18 19:46:51 +0100

    fix: spelling errors and return notes on empty notesFilter

--- client/src/selectors/notesSelectors.js
+++ client/src/selectors/notesSelectors.js
@@ -1,21 +1,21 @@
 import { createSelector } from 'reselect'
 
-const getNoteFilter = state => state.noteFilter
+const getNoteFilter = state => state.notesFilter
 const getNotes = state => state.notes
 
 export const getFilteredNotes = createSelector(
   [getNoteFilter, getNotes],
-  (noteFilter, notes) => {
-    if (noteFilter !== '') {
+  (notesFilter, notes) => {
+    if (notesFilter !== '') {
       const filtered = {}
       for (const id in notes) {
         // find out if note matches filter
-        if (notes[id].content.includes(noteFilter)) {
+        if (notes[id].content.includes(notesFilter)) {
           filtered[id] = notes[id]
         }
       }
       return filtered
     }
-    return getNotes
+    return notes
   }
 )
